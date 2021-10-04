07e01a8e0639d0ea6f7cf7b0a34e6cd331d32e0e
Courtney Ring <courtney.ring@imagination.com>
2021-10-04 15:15:03 -0400

    add pictures, adjust hours

--- app/components/exhibits/exhibitsController.js
+++ app/components/exhibits/exhibitsController.js
@@ -1,5 +1,12 @@
 app.controller('exhibitsController', function($scope, $location) {
     $scope.currentPage = '/#'+$location.url()
 
-    
+    $scope.range = function(min, max, step) {
+        step = step || 1;
+        var input = [];
+        for (var i = min; i <= max; i += step) {
+            input.push(i);
+        }
+        return input;
+    };
 })
